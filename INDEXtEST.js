e7f0eaae214d915b2d7f580eed3c1f55a95b9231
DESKTOP-4I9JKHI\Lenovo <aliadil@xpertdigital.co>
2022-10-17 19:16:01 +0500

    Star Code with Array

---
+++ INDEXtEST.js
@@ -0,0 +1,29 @@
+let arry = [];
+var values = 30;
+var valuePrint = "*";
+
+for (let i = 0 ; i <values ; i++){
+    for (let l = 0 ; l <i; l++){   
+       var m = ""
+      
+       for(let b = values;  b > 0; b--){
+          m  += " " + "";
+         }
+
+         values = values-1;
+          arry[0] = m;
+        arry.push(valuePrint);
+        var myvalue = 
+        console.log(""+arry);
+   
+       }  
+}
+
+for (let k= arry.length ; k >0; k--){
+   m  += " " + "";
+   arry[0] = m;
+  arry.pop(valuePrint)
+  console.log(""+arry);
+
+  }
+
